7de83d9f413d3de0c6864485de8ffe8295011540
KRISHNA <krishnapareek049@gmail.com>
2026-01-05 19:15:59 +0530

    learning management system website

---
+++ src/pages/educator/Dashboard.jsx
@@ -0,0 +1,16 @@
+import React, { useContext, useState } from 'react'
+import { AppContext } from '../../context/AppContext';
+
+const Dashboard = () => {
+
+  const {currency} = useContext(AppContext);
+  const [dashboardData,setDashboardData] = useState(null);
+
+  return (
+    <div>
+        <h1>Educator Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard
